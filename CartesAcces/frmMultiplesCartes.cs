a6f93e3d652c88bdcfc8fc6299e9a433e0a69243
ian.bellot001@gmail.com <ian.bellot001@gmail.com>
2023-01-10 13:56:10 +0100

    commit

--- CartesAcces/frmMultiplesCartes.cs
+++ CartesAcces/frmMultiplesCartes.cs
@@ -1,12 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace CartesAcces
@@ -22,7 +16,7 @@ namespace CartesAcces
         {
             InitializeComponent();
             Eleve.setLesClasses(cbbImprClasse);
-            Grid.checkedSectionClasse();
+            checkedSectionClasse();
         }
 
         public void checkedSectionClasse()
