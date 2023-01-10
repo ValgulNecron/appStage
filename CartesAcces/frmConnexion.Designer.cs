41473086747ea30d84c39953bc265ae68b9165ca
ian.bellot001@gmail.com <ian.bellot001@gmail.com>
2023-01-10 08:50:31 +0100

    Form connexion

---
+++ CartesAcces/frmConnexion.Designer.cs
@@ -0,0 +1,105 @@
+﻿using System.ComponentModel;
+
+namespace CartesAcces
+{
+    partial class frmConnexion
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConnexion = new System.Windows.Forms.Button();
+            this.lblIdent = new System.Windows.Forms.Label();
+            this.lblMdp = new System.Windows.Forms.Label();
+            this.txtIdentifiant = new System.Windows.Forms.TextBox();
+            this.txtMotDePasse = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnConnexion
+            // 
+            this.btnConnexion.Location = new System.Drawing.Point(284, 339);
+            this.btnConnexion.Name = "btnConnexion";
+            this.btnConnexion.Size = new System.Drawing.Size(75, 23);
+            this.btnConnexion.TabIndex = 0;
+            this.btnConnexion.Text = "Connexion";
+            this.btnConnexion.UseVisualStyleBackColor = true;
+            this.btnConnexion.Click += new System.EventHandler(this.btnConnexion_Click);
+            // 
+            // lblIdent
+            // 
+            this.lblIdent.Location = new System.Drawing.Point(147, 139);
+            this.lblIdent.Name = "lblIdent";
+            this.lblIdent.Size = new System.Drawing.Size(100, 23);
+            this.lblIdent.TabIndex = 1;
+            this.lblIdent.Text = "Identifiant";
+            // 
+            // lblMdp
+            // 
+            this.lblMdp.Location = new System.Drawing.Point(147, 202);
+            this.lblMdp.Name = "lblMdp";
+            this.lblMdp.Size = new System.Drawing.Size(100, 23);
+            this.lblMdp.TabIndex = 2;
+            this.lblMdp.Text = "Mot de passe";
+            // 
+            // txtIdentifiant
+            // 
+            this.txtIdentifiant.Location = new System.Drawing.Point(223, 136);
+            this.txtIdentifiant.Name = "txtIdentifiant";
+            this.txtIdentifiant.Size = new System.Drawing.Size(136, 20);
+            this.txtIdentifiant.TabIndex = 3;
+            // 
+            // txtMotDePasse
+            // 
+            this.txtMotDePasse.Location = new System.Drawing.Point(223, 199);
+            this.txtMotDePasse.Name = "txtMotDePasse";
+            this.txtMotDePasse.Size = new System.Drawing.Size(136, 20);
+            this.txtMotDePasse.TabIndex = 4;
+            // 
+            // frmConnexion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtMotDePasse);
+            this.Controls.Add(this.txtIdentifiant);
+            this.Controls.Add(this.lblMdp);
+            this.Controls.Add(this.lblIdent);
+            this.Controls.Add(this.btnConnexion);
+            this.Name = "frmConnexion";
+            this.Text = "frmConnexion";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.Button btnConnexion;
+        private System.Windows.Forms.Label lblIdent;
+        private System.Windows.Forms.Label lblMdp;
+        private System.Windows.Forms.TextBox txtIdentifiant;
+        private System.Windows.Forms.TextBox txtMotDePasse;
+
+        #endregion
+    }
+}
