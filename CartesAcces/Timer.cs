4791b0dc131279107330e1b11cc51cd9c6b63a01
Valgul <charlieryannnonpro@gmail.com>
2023-01-10 11:39:55 +0100

    Timer.cs

--- CartesAcces/Timer.cs
+++ CartesAcces/Timer.cs
@@ -1,11 +1,28 @@
-﻿namespace CartesAcces
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace CartesAcces
 {
     public class Timer
     {
         private DateTime start;
         private Timer timer;
         private int dureeMinute = 15;
-        
-        public Timer(int minute, DateTime date)
+
+        public Timer(Form form)
+        {
+            start = DateTime.Now;
+            timer = new Timer();
+            timer.Interval = dureeMinute / 60 / 1000; // la duree en minute mit en ms pour l'interval
+            timer.Tick = +OnTimerTick;
+
+            form.MouseMove += Form_MouseMove;
+        }
+
+        private void Form_MouseMove(object sender, MouseEventArgs e)
+        {
+            start = DateTime.Now;
+        }
     }
 }
