051b25d077d2874ce51fcc05d05dd6cf16c33981
Valgul <charlieryannnonpro@gmail.com>
2023-01-10 09:37:17 +0100

    Globale.cs

---
+++ CartesAcces/Globale.cs
@@ -0,0 +1,20 @@
+﻿namespace CartesAcces
+{
+    public static class Globale
+    {
+        static bool _estConnecter = false;
+        static bool _estEnModeSombre = false;
+        private static string _nomUtilisateur = "";
+        private static int _dureeConnexion = 0;
+
+        static public string _couleurDeFondClaire = "#eff1f5";
+        static public string _couleurDuTexteclaire = "#4c4f69";
+        static public string _couleurBandeauxClaire = "#179299";
+        static public string _couleurBoutonsClaire = "#8c8fa1";
+        
+        static public string _couleurDeFondSombre = "#1e1e2e";
+        static public string _couleurDuTexteSombre = "#cdd6f4";
+        static public string _couleurBandeauxSombre = "#94e2d5";
+        static public string _couleurBoutonsSombre = "#9399b2";
+    }
+}
