e304f5237859233422f88480719681a40b63f2bd
ian.bellot001@gmail.com <ian.bellot001@gmail.com>
2023-01-12 09:28:32 +0100

    Installation itext7

--- CartesAcces/Pdf.cs
+++ CartesAcces/Pdf.cs
@@ -1,7 +1,20 @@
 namespace CartesAcces
 {
-    public class Pdf
+    public static class Pdf
     {
-        
+        public static void getText()
+        {
+            string path = "./1SLAM.pdf";
+            PdfDocument pdfDoc = new PdfDocument(new PdfReader(path));
+            Document doc = new Document(pdfDoc);
+            for (int page = 1; page <= pdfDoc.GetNumberOfPages(); page++)
+            {
+                ITextExtractionStrategy strategy = new SimpleTextExtractionStrategy();
+                string pageContent = PdfTextExtractor.GetTextFromPage(pdfDoc.GetPage(page), strategy);
+                Console.WriteLine(pageContent);
+            }
+            pdfDoc.Close();
+            Console.ReadKey();
+        }
     }
 }
