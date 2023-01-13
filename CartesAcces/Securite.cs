80167deb923e75eb023ad5b18107ab0387f64c32
Valgul <charlieryannnonpro@gmail.com>
2023-01-13 09:16:37 +0100

    dossier celon la classe

--- CartesAcces/Securite.cs
+++ CartesAcces/Securite.cs
@@ -1,45 +1,46 @@
 ﻿using System;
 using System.Security.Cryptography;
 using System.Text.RegularExpressions;
+
 namespace CartesAcces
 {
     public static class Securite
     {
-        
         public static DateTime derniereUtilisation = DateTime.Now;
+
         public static string creationHash(string motDePasse)
         {
             //on crée le sel qui permettra au mot de passe d'avoir un hash unique et différent à chaque fois meme si le mot de passe est le meme
-            byte[] salt = new byte[16];
+            var salt = new byte[16];
             new RNGCryptoServiceProvider().GetBytes(salt);
 
             //on cree le hash du mot de passe avec le sel 
             var pbkdf2 = new Rfc2898DeriveBytes(motDePasse, salt, 100000);
-            byte[] hash = pbkdf2.GetBytes(20);
+            var hash = pbkdf2.GetBytes(20);
 
             //on concatene le hash et le sel pour avoir un hash unique 
-            byte[] hashBytes = new byte[36];
+            var hashBytes = new byte[36];
             Array.Copy(salt, 0, hashBytes, 0, 16);
             Array.Copy(hash, 0, hashBytes, 16, 20);
 
             //on retourne le hash sous forme string encoder en base64 
-            string savedPasswordHash = Convert.ToBase64String(hashBytes);
+            var savedPasswordHash = Convert.ToBase64String(hashBytes);
             return savedPasswordHash;
         }
 
         public static bool verificationHash(string motDePasse, string savedPasswordHash)
         {
             //on recupere le hash et le sel du mot de passe enregistréw
-            byte[] hashBytes = Convert.FromBase64String(savedPasswordHash);
-            byte[] salt = new byte[16];
+            var hashBytes = Convert.FromBase64String(savedPasswordHash);
+            var salt = new byte[16];
             Array.Copy(hashBytes, 0, salt, 0, 16);
 
             //on recree le hash du mot de passe entré avec le sel du mot de passe enregistré
             var pbkdf2 = new Rfc2898DeriveBytes(motDePasse, salt, 100000);
-            byte[] hash = pbkdf2.GetBytes(20);
+            var hash = pbkdf2.GetBytes(20);
 
             //on compare le hash du mot de passe entré avec le hash du mot de passe enregistré
-            for (int i = 0; i < 20; i++)
+            for (var i = 0; i < 20; i++)
                 if (hashBytes[i + 16] != hash[i])
                     //si le hash est different on envoie une erreur
                     throw new UnauthorizedAccessException();
@@ -56,15 +57,14 @@ namespace CartesAcces
 
             if (!Regex.IsMatch(motDePasse, @"[A-Z]"))
                 return false;
-            
+
             if (!Regex.IsMatch(motDePasse, @"[0-9]"))
                 return false;
 
             if (!Regex.IsMatch(motDePasse, @"[!@#$%^&*()_+=\[{\]};:<>|./?,-~\\]"))
-                return false; 
-            
+                return false;
+
             return true;
         }
     }
-    
 }
