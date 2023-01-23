a908afcce21155713d261f5a32e326f835c65253
march67 <td.ta.david@gmail.com>
2023-01-23 09:21:20 +0100

    using deleted

--- CarteAccesLib/Couleur.cs
+++ CarteAccesLib/Couleur.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Drawing;
 using System.Windows.Forms;
-using System.Windows.Forms.VisualStyles;
 
 //using Org.BouncyCastle.Asn1.Crmf;
 
--- CartesAcces/frmParamtres.cs
+++ CartesAcces/frmParamtres.cs
@@ -1,9 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
-using System.Linq;
 using System.Threading;
 using System.Windows.Forms;
 using CartesAcces.Properties;
