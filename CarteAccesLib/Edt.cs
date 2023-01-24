59d6a94d9559f924d4fe87d6a2ed88c541552028
ShortLegsFox <mister.dusbin777@gmail.com>
2023-01-24 09:09:22 +0100

    Séparation de la classe edition

--- CarteAccesLib/Edt.cs
+++ CarteAccesLib/Edt.cs
@@ -1,44 +1,13 @@
-using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Drawing2D;
-using System.Drawing.Imaging;
-using System.IO;
-using System.Runtime.InteropServices;
-using System.Threading;
 using System.Windows.Forms;
-using CarteAccesLib;
 using CartesAcces;
 
 namespace CarteAcces
 {
     public static class Edt
     {
-        // ** VARIABLES : Pour l'édition de l'emploi du temps (rognage) **
-        public static bool
-            selectionClick = false; // -> Est ce que le bouton "Selectionner" a été cliqué ? Si oui passe à true
-
-        public static int cropX; // -> Abscisse de départ du rognage
-        public static int cropY; // -> Ordonnée de départ du rognage
-        public static int cropWidth; // -> Largeur du rognage
-        public static int cropHeight; // -> Hauteur du rognage
-        public static Pen cropPen; // -> Stylo qui dessine le rectangle correspondant au rognage
-
-        // ** VARIABLES  : Déplacement de la photo
-        public static bool
-            drag = false; // -> Est ce que l'utilisateur a cliqué sur la photo ? (clique maintenue : drag passera a true)
-
-        public static int
-            posX; // -> Abscisse initiale, sauvegardée quand l'utilisateur commence le déplacement (clic maintenu sur la photo)
-
-        public static int
-            posY; // -> Ordonnée initialie, sauvegardée quand l'utilisateur commence le déplacement (clic maintenu sur la photo)
-
-        // ** VARIABLES : Chemin de l'image **
-        public static string FilePath;
-
-        public static string cheminImpressionFinal;
-        
         // -- Selection du bon emploi du temps en fonction de la classe selectionnée
         public static void afficheEmploiDuTemps(ComboBox cbbClasse, PictureBox pbCarteArriere)
         {
@@ -72,17 +41,17 @@ namespace CarteAcces
         public static void cropEdt(PictureBox pbCarteArriere, string pathEdt)
         {
             // -- Si la largeur a rogner est trop faible, on sort --
-            if (cropWidth < 1) return;
+            if (Edition.cropWidth < 1) return;
 
             /* -- Rectangle pour stocker l'image rognée avec les points calculés --
                 Les dimensions calculées ci dessous utilisent les dimensions 920 x 604 (calcul par proportionnalité)
                 qui sont celles des vrai fichier EDT !
                 Cela permet d'éviter les problèmes de résolution d'image après le rognage */
 
-            var cropWidthReal = cropWidth * pbCarteArriere.Image.Width / 540;
-            var cropHeightReal = cropHeight * pbCarteArriere.Image.Height / 354;
-            var cropXReal = cropX * pbCarteArriere.Image.Width / 540;
-            var cropYReal = cropY * pbCarteArriere.Image.Height / 354;
+            var cropWidthReal = Edition.cropWidth * pbCarteArriere.Image.Width / 540;
+            var cropHeightReal = Edition.cropHeight * pbCarteArriere.Image.Height / 354;
+            var cropXReal = Edition.cropX * pbCarteArriere.Image.Width / 540;
+            var cropYReal = Edition.cropY * pbCarteArriere.Image.Height / 354;
 
             var rect = new Rectangle(cropXReal, cropYReal, cropWidthReal, cropHeightReal);
 
@@ -125,9 +94,5 @@ namespace CarteAcces
                     // Next ..
                 }
         }
-        
-        
-
-        
     }
 }
--- CarteAccesLib/Photo.cs
+++ CarteAccesLib/Photo.cs
@@ -1,48 +1,19 @@
-using System;
-using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Drawing2D;
 using System.Drawing.Imaging;
 using System.IO;
-using System.Runtime.InteropServices;
 using System.Threading;
 using System.Windows.Forms;
-using CarteAccesLib;
 using CartesAcces;
 
 namespace CarteAcces
 {
     public static class Photo
     {
-        // ** VARIABLES : Pour l'édition de l'emploi du temps (rognage) **
-        public static bool
-            selectionClick = false; // -> Est ce que le bouton "Selectionner" a été cliqué ? Si oui passe à true
-
-        public static int cropX; // -> Abscisse de départ du rognage
-        public static int cropY; // -> Ordonnée de départ du rognage
-        public static int cropWidth; // -> Largeur du rognage
-        public static int cropHeight; // -> Hauteur du rognage
-        public static Pen cropPen; // -> Stylo qui dessine le rectangle correspondant au rognage
-
-        // ** VARIABLES  : Déplacement de la photo
-        public static bool
-            drag = false; // -> Est ce que l'utilisateur a cliqué sur la photo ? (clique maintenue : drag passera a true)
-
-        public static int
-            posX; // -> Abscisse initiale, sauvegardée quand l'utilisateur commence le déplacement (clic maintenu sur la photo)
-
-        public static int
-            posY; // -> Ordonnée initialie, sauvegardée quand l'utilisateur commence le déplacement (clic maintenu sur la photo)
-
-        // ** VARIABLES : Chemin de l'image **
-        public static string FilePath;
-
-        public static string cheminImpressionFinal;
-        
         public static void cropLaPhoto(PictureBox pbPhotoUnique)
         {
             // -- Si la largeur a rogner est trop faible, on sort --
-            if (cropWidth < 1) return;
+            if (Edition.cropWidth < 1) return;
 
             /* -- Rectangle pour stocker l'image rognée avec les points calculés --
                 Les dimensions calculées ci dessous utilisent les dimensions 920 x 604 (calcul par proportionnalité)
@@ -52,15 +23,15 @@ namespace CarteAcces
             var widthSave = pbPhotoUnique.Width;
             var heightSave = pbPhotoUnique.Height;
 
-            var cropWidthReal = cropWidth * pbPhotoUnique.Image.Width / pbPhotoUnique.Width;
-            var cropHeightReal = cropHeight * pbPhotoUnique.Image.Height / pbPhotoUnique.Height;
-            var cropXReal = cropX * pbPhotoUnique.Image.Width / pbPhotoUnique.Width;
-            var cropYReal = cropY * pbPhotoUnique.Image.Height / pbPhotoUnique.Height;
+            var cropWidthReal = Edition.cropWidth * pbPhotoUnique.Image.Width / pbPhotoUnique.Width;
+            var cropHeightReal = Edition.cropHeight * pbPhotoUnique.Image.Height / pbPhotoUnique.Height;
+            var cropXReal = Edition.cropX * pbPhotoUnique.Image.Width / pbPhotoUnique.Width;
+            var cropYReal = Edition.cropY * pbPhotoUnique.Image.Height / pbPhotoUnique.Height;
 
             var rect = new Rectangle(cropXReal, cropYReal, cropWidthReal, cropHeightReal);
 
             // -- On stock l'image original dans un bitmap --
-            var OriginalImage = new Bitmap(Image.FromFile(FilePath));
+            var OriginalImage = new Bitmap(Image.FromFile(Edition.FilePath));
 
             // -- Bitmap pour l'image rognée --
             var _img = new Bitmap(cropWidthReal, cropHeightReal);
