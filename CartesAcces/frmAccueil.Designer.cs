856ae740e9b01e2485e8518d8b6698a7df00c13a
ShortLegsFox <mister.dusbin777@gmail.com>
2023-01-24 09:14:24 +0100

    Main window normal state

--- CartesAcces/frmAccueil.Designer.cs
+++ CartesAcces/frmAccueil.Designer.cs
@@ -194,7 +194,7 @@ namespace CartesAcces
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "frmAccueil";
             this.Text = "Application Cartes Acces";
-            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
             this.Load += new System.EventHandler(this.frmAccueil_Load);
             this.pnlMenu.ResumeLayout(false);
             this.pnlMenu.PerformLayout();
