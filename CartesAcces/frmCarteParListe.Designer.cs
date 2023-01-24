035cdae38c21007267bfa445607cc53468222a7e
ShortLegsFox <mister.dusbin777@gmail.com>
2023-01-24 08:18:48 +0100

    Rename frmListeEleve.cs

--- CartesAcces/frmCarteParListe.Designer.cs
+++ CartesAcces/frmCarteParListe.Designer.cs
@@ -36,6 +36,9 @@ namespace CartesAcces
             this.Liste = new System.Windows.Forms.ListBox();
             this.txtRechercher = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
+            this.Impression = new System.Windows.Forms.ListBox();
+            this.btnValider = new System.Windows.Forms.Button();
+            this.btnAjout = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -78,22 +81,56 @@ namespace CartesAcces
             this.label2.Text = "Rechercher : ";
             this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
+            // Impression
+            // 
+            this.Impression.FormattingEnabled = true;
+            this.Impression.Location = new System.Drawing.Point(445, 70);
+            this.Impression.Name = "Impression";
+            this.Impression.Size = new System.Drawing.Size(343, 355);
+            this.Impression.TabIndex = 5;
+            // 
+            // btnValider
+            // 
+            this.btnValider.Location = new System.Drawing.Point(713, 431);
+            this.btnValider.Name = "btnValider";
+            this.btnValider.Size = new System.Drawing.Size(75, 23);
+            this.btnValider.TabIndex = 6;
+            this.btnValider.Text = "Valider";
+            this.btnValider.UseVisualStyleBackColor = true;
+            // 
+            // btnAjout
+            // 
+            this.btnAjout.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAjout.Location = new System.Drawing.Point(376, 203);
+            this.btnAjout.Name = "btnAjout";
+            this.btnAjout.Size = new System.Drawing.Size(63, 32);
+            this.btnAjout.TabIndex = 7;
+            this.btnAjout.Text = "->";
+            this.btnAjout.UseVisualStyleBackColor = true;
+            // 
             // frmListeEleve
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 466);
+            this.Controls.Add(this.btnAjout);
+            this.Controls.Add(this.btnValider);
+            this.Controls.Add(this.Impression);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.txtRechercher);
             this.Controls.Add(this.Liste);
             this.Controls.Add(this.lblNombre);
             this.Controls.Add(this.label1);
             this.Name = "frmListeEleve";
-            this.Text = "frmListeEleve";
-            this.Load += new System.EventHandler(this.frmListeEleve_Load);
+            this.Text = "Cartes par liste personnalisée";
+            this.Load += new System.EventHandler(this.frmCartesParListe_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
         }
+
+        private System.Windows.Forms.ListBox Impression;
+        private System.Windows.Forms.Button btnValider;
+        private System.Windows.Forms.Button btnAjout;
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label lblNombre;
