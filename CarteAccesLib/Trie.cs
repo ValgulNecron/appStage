25e0f6fad085fd14501ae3aa0885ebc49f11ffaf
valgul <valgulnecron@gmail.com>
2023-01-25 08:41:23 +0100

    trie
    Signed-off-by: valgul <valgulnecron@gmail.com>

--- CarteAccesLib/Trie.cs
+++ CarteAccesLib/Trie.cs
@@ -1,7 +1,26 @@
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using CartesAcces;
+
 namespace CarteAccesLib
 {
-    public class Trie
+    public static class Trie
     {
-        
+        public static List<Eleve> search(List<Eleve> listeEleve, string filtre)
+        {
+            var listeEleveResultat = new List<Eleve>();
+            Regex regex = new Regex(filtre);
+            foreach (Eleve eleve in listeEleve)
+            {
+                string nomPrenom = eleve.NomEleve + " " + eleve.PrenomEleve;
+                Match match = regex.Match(nomPrenom);
+                if (match.Success)
+                {
+                    listeEleveResultat.Add(eleve);
+                }
+            }
+
+            return listeEleveResultat;
+        }
     }
 }
