c83ee7f18047b2be4d7ee6fb68afc28cc3dce5bb
ShortLegsFox <mister.dusbin777@gmail.com>
2023-01-25 09:28:41 +0100

    Tentative 1

--- CartesAcces/frmRognageEdtClassique.cs
+++ CartesAcces/frmRognageEdtClassique.cs
@@ -1,5 +1,9 @@
 using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
 using System.Windows.Forms;
+using CarteAcces;
 
 namespace CartesAcces
 {
@@ -12,7 +16,10 @@ namespace CartesAcces
 
         private void frmRognageEdtClassique_Load(object sender, EventArgs e)
         {
-            pbEdtClassique.Image = null;
+            List<string> listeFichiers = new List<string>();
+            listeFichiers.AddRange(Directory.GetFiles(Globale._cheminEdtClassique));
+            pbEdtClassique.Image = Image.FromFile(listeFichiers[0]);
+            Edt.rognageEdt(pbEdtClassique, Globale._cheminEdtClassique);
         }
     }
 }
