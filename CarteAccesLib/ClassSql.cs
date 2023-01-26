1bc2e0b21240eb58d59d1a1631ee01ae3b37ae81
valgul <valgulnecron@gmail.com>
2023-01-26 13:00:27 +0100

    aaa
    Signed-off-by: valgul <valgulnecron@gmail.com>

--- CarteAccesLib/ClassSql.cs
+++ CarteAccesLib/ClassSql.cs
@@ -1,33 +1,72 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Data;
-using System.Data.Entity;
 using System.Xml;
-using MySql.Data.MySqlClient;
+using Microsoft.EntityFrameworkCore;
 
 namespace CartesAcces
 {
-    public static class ClassSql
+    public class ClassSql : DbContext
     {
+        public System.Data.Entity.DbSet<Utilisateur> Utilisateurs { get; set; }
+        public System.Data.Entity.DbSet<LogAction> LogActions { get; set; }
+        public System.Data.Entity.DbSet<LogMotDePasse> LogMotDePasses { get; set; }
 
-        public static void init()
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            var configFile = "./config.xml";
             var doc = new XmlDocument();
-            doc.Load(configFile);
-            var nodeIp = doc.SelectSingleNode("/configuration/appSettings/add[@key='IP']");
-            var ip = nodeIp.Attributes["value"].Value;
-            var nodeBd = doc.SelectSingleNode("/configuration/appSettings/add[@key='BD']");
-            var bd = nodeBd.Attributes["value"].Value;
-            var nodeUtilisateur = doc.SelectSingleNode("/configuration/appSettings/add[@key='UTILISATEUR']");
-            var utilisateur = nodeUtilisateur.Attributes["value"].Value;
-            var nodeMotDePasse = doc.SelectSingleNode("/configuration/appSettings/add[@key='MOTDEPASSE']");
-            var motDePasse = nodeMotDePasse.Attributes["value"].Value;
-            var sqlCreateur = new MySqlConnectionStringBuilder();
-            sqlCreateur.Server = ip;
-            sqlCreateur.Port = 3306;
-            sqlCreateur.Database = bd;
-            sqlCreateur.UserID = utilisateur;
-            sqlCreateur.Password = motDePasse;
-            var texteConnexion = sqlCreateur.ConnectionString;
+            doc.Load("./config.xml");
+            string mariaDb = "";
+            var node = doc.SelectSingleNode("/appSettings/add[@key='IP']");
+            if (node.Attributes != null) mariaDb += "Server" + node.Attributes["value"].Value + ";";
+            node = doc.SelectSingleNode("/appSettings/add[@key='DB']");
+            if (node.Attributes != null) mariaDb += "Database" + node.Attributes["value"].Value + ";";
+            node = doc.SelectSingleNode("/appSettings/add[@key='UTILISATEUR']");
+            if (node.Attributes != null) mariaDb += "Uid" + node.Attributes["value"].Value + ";";
+            node = doc.SelectSingleNode("/appSettings/add[@key='MOTDEPASSE']");
+            if (node.Attributes != null) mariaDb += "Pwd" + node.Attributes["value"].Value + ";";
+            optionsBuilder.UseMySql(mariaDb);
         }
     }
+    
+    public class Utilisateur
+    {
+        [Key]
+        public string NomUtilisateur { get; set; }
+        public string Hash { get; set; }
+        public bool ThemeBool { get; set; }
+    }
+    
+    public class LogAction
+    {
+        [Key]
+        [Column(Order = 0)]
+        public DateTime DateAction { get; set; }
+
+        [Key]
+        [Column(Order = 1)]
+        public string NomUtilisateur { get; set; }
+        public string Action { get; set; }
+        public string AdMac { get; set; }
+
+        [ForeignKey("NomUtilisateur")]
+        public Utilisateur Utilisateur { get; set; }
+    }
+    
+    public class LogMotDePasse
+    {
+        [Key]
+        [Column(Order = 0)]
+        public DateTime DateLogMotDePasse { get; set; }
+        [Key]
+        [Column(Order = 1)]
+        public string NomUtilisateur { get; set; }
+        public string Hash { get; set; }
+
+        [ForeignKey("Hash")]
+        public Utilisateur Utilisateur { get; set; }
+    }
+    
+    
 }
