0e694b53f7bb7f7712ddf6bdf6594a1fc5417f6b
ShortLegsFox <mister.dusbin777@gmail.com>
2023-01-27 08:54:36 +0100

    Usings inutiles

--- CartesAcces/frmCarteProvisoire.cs
+++ CartesAcces/frmCarteProvisoire.cs
@@ -1,14 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Drawing2D;
-using System.Drawing.Imaging;
-using System.IO;
-using System.Runtime.InteropServices;
 using System.Windows.Forms;
 using CarteAcces;
 using CarteAccesLib;
-using Application = Microsoft.Office.Interop.Word.Application;
 
 namespace CartesAcces
 {
