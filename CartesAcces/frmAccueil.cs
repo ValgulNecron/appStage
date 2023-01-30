c2bf197dc69b46e2e91f72fda131819119004861
valgul <valgulnecron@gmail.com>
2023-01-30 09:08:30 +0100

    aaa

--- CartesAcces/frmAccueil.cs
+++ CartesAcces/frmAccueil.cs
@@ -67,21 +67,21 @@ namespace CartesAcces
             lblVersion.Text = "version :" + Globale._version + " du " + Globale._versionDate;
             var time = new Timer(this);
             var dir = new DirectoryInfo("./data/image");
-            if (dir.LastWriteTime.Add(TimeSpan.FromDays(15)) <= DateTime.Now)
+            if (dir.CreationTime.Add(TimeSpan.FromDays(15)) <= DateTime.Now)
             {
-                MessageBox.Show("7j depuis le denier import des edt");
+                MessageBox.Show("15j depuis le denier import des edt");
             }
             
             var dir2 = new DirectoryInfo(Chemin.cheminPhotoEleve);
-            if (dir2.LastWriteTime.Add(TimeSpan.FromDays(15)) <= DateTime.Now)
+            if (dir2.CreationTime.Add(TimeSpan.FromDays(15)) <= DateTime.Now)
             {
-                MessageBox.Show("7j depuis le dernier import de photo");
+                MessageBox.Show("15j depuis le dernier import de photo");
             }
 
             var dir3 = new DirectoryInfo(Chemin.cheminListeEleve);
-            if (dir3.LastWriteTime.Add(TimeSpan.FromDays(15)) <= DateTime.Now)
+            if (dir3.CreationTime.Add(TimeSpan.FromDays(15)) <= DateTime.Now)
             {
-                MessageBox.Show("7j depuis le dernier import des photo");
+                MessageBox.Show("15j depuis le dernier import des listes eleves");
             }
         }
 
