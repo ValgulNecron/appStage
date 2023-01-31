e855832e0f0fe52d84a2c72bb40cb1e1ba43478f
valgul <valgulnecron@gmail.com>
2023-01-31 15:04:56 +0100

    aaaa

--- CartesAcces/frmChangeMotDePasse.cs
+++ CartesAcces/frmChangeMotDePasse.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Windows.Forms;
 using LinqToDB;
 
@@ -34,6 +35,22 @@ namespace CartesAcces
                     {
                         if (nouveauMdp.Text == nouveauMdpValid.Text)
                         {
+                            string macAddress = string.Empty;
+                            foreach (NetworkInterface nic in NetworkInterface.GetAllNetworkInterfaces())
+                            {
+                                // Only consider Ethernet network interfaces, thereby ignoring any loopback network interfaces
+                                if ((nic.NetworkInterfaceType == NetworkInterfaceType.Ethernet || nic.NetworkInterfaceType == NetworkInterfaceType.Wireless80211) &&
+                                    nic.OperationalStatus == OperationalStatus.Up)
+                                {
+                                    macAddress += nic.GetPhysicalAddress().ToString();
+                                    break;
+                                }
+                            }
+                            var log = new LogAction();
+                            log.DateAction = DateTime.Now;
+                            log.Utilisateur = user;
+                            log.Action = "Changement de mot de passe";
+                            log.AdMac = macAddress;
                             user.Hash = Securite.creationHash(nouveauMdp.Text);
                             ClassSql.db.Update(user);
                             MessageBox.Show("mot de passe changé");
