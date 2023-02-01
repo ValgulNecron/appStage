71973dbbc982fc6b945fbf56ae3f5e03a054986d
ShortLegsFox <mister.dusbin777@gmail.com>
2023-02-01 08:13:28 +0100

    Nouvelle form : ImportEtablissement.cs

---
+++ CartesAcces/ImportEtablissement.Designer.cs
@@ -0,0 +1,235 @@
+using System.ComponentModel;
+
+namespace CartesAcces
+{
+    partial class ImportEtablissement
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label11 = new System.Windows.Forms.Label();
+            this.txtVilleEtablissement = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtMailEtablissement = new System.Windows.Forms.TextBox();
+            this.txtTelEtablissement = new System.Windows.Forms.TextBox();
+            this.txtCodePostalEtablissement = new System.Windows.Forms.TextBox();
+            this.txtNumRueEtablissement = new System.Windows.Forms.TextBox();
+            this.txtRueEtablissement = new System.Windows.Forms.TextBox();
+            this.txtNomEtablissement = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnValider = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label11
+            // 
+            this.label11.Location = new System.Drawing.Point(35, 117);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(35, 23);
+            this.label11.TabIndex = 64;
+            this.label11.Text = "Ville :";
+            // 
+            // txtVilleEtablissement
+            // 
+            this.txtVilleEtablissement.Location = new System.Drawing.Point(70, 114);
+            this.txtVilleEtablissement.Name = "txtVilleEtablissement";
+            this.txtVilleEtablissement.Size = new System.Drawing.Size(149, 20);
+            this.txtVilleEtablissement.TabIndex = 63;
+            // 
+            // label10
+            // 
+            this.label10.Location = new System.Drawing.Point(35, 223);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(80, 23);
+            this.label10.TabIndex = 62;
+            this.label10.Text = "Adresse mail : ";
+            // 
+            // label9
+            // 
+            this.label9.Location = new System.Drawing.Point(35, 194);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(115, 23);
+            this.label9.TabIndex = 61;
+            this.label9.Text = "Numéro de téléphone : ";
+            // 
+            // label8
+            // 
+            this.label8.Location = new System.Drawing.Point(225, 117);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(70, 23);
+            this.label8.TabIndex = 60;
+            this.label8.Text = "Code Postal : ";
+            // 
+            // label6
+            // 
+            this.label6.Location = new System.Drawing.Point(35, 94);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(84, 23);
+            this.label6.TabIndex = 59;
+            this.label6.Text = "Numéro de rue :";
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(163, 94);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(33, 23);
+            this.label4.TabIndex = 58;
+            this.label4.Text = "Rue :\r\n";
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(35, 30);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(122, 23);
+            this.label3.TabIndex = 57;
+            this.label3.Text = "Nom de l\'établissement : ";
+            // 
+            // txtMailEtablissement
+            // 
+            this.txtMailEtablissement.Location = new System.Drawing.Point(119, 220);
+            this.txtMailEtablissement.Name = "txtMailEtablissement";
+            this.txtMailEtablissement.Size = new System.Drawing.Size(176, 20);
+            this.txtMailEtablissement.TabIndex = 56;
+            // 
+            // txtTelEtablissement
+            // 
+            this.txtTelEtablissement.Location = new System.Drawing.Point(156, 191);
+            this.txtTelEtablissement.Name = "txtTelEtablissement";
+            this.txtTelEtablissement.Size = new System.Drawing.Size(101, 20);
+            this.txtTelEtablissement.TabIndex = 55;
+            // 
+            // txtCodePostalEtablissement
+            // 
+            this.txtCodePostalEtablissement.Location = new System.Drawing.Point(297, 114);
+            this.txtCodePostalEtablissement.Name = "txtCodePostalEtablissement";
+            this.txtCodePostalEtablissement.Size = new System.Drawing.Size(81, 20);
+            this.txtCodePostalEtablissement.TabIndex = 54;
+            // 
+            // txtNumRueEtablissement
+            // 
+            this.txtNumRueEtablissement.Location = new System.Drawing.Point(125, 91);
+            this.txtNumRueEtablissement.Name = "txtNumRueEtablissement";
+            this.txtNumRueEtablissement.Size = new System.Drawing.Size(32, 20);
+            this.txtNumRueEtablissement.TabIndex = 53;
+            // 
+            // txtRueEtablissement
+            // 
+            this.txtRueEtablissement.Location = new System.Drawing.Point(202, 91);
+            this.txtRueEtablissement.Name = "txtRueEtablissement";
+            this.txtRueEtablissement.Size = new System.Drawing.Size(176, 20);
+            this.txtRueEtablissement.TabIndex = 52;
+            // 
+            // txtNomEtablissement
+            // 
+            this.txtNomEtablissement.Location = new System.Drawing.Point(163, 27);
+            this.txtNomEtablissement.Name = "txtNomEtablissement";
+            this.txtNomEtablissement.Size = new System.Drawing.Size(215, 20);
+            this.txtNomEtablissement.TabIndex = 51;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(35, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 28);
+            this.label1.TabIndex = 65;
+            this.label1.Text = "Adresse\r\n";
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(35, 166);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(122, 22);
+            this.label2.TabIndex = 66;
+            this.label2.Text = "Coordonnées";
+            // 
+            // btnValider
+            // 
+            this.btnValider.Location = new System.Drawing.Point(303, 288);
+            this.btnValider.Name = "btnValider";
+            this.btnValider.Size = new System.Drawing.Size(75, 23);
+            this.btnValider.TabIndex = 67;
+            this.btnValider.Text = "Valider";
+            this.btnValider.UseVisualStyleBackColor = true;
+            // 
+            // ImportEtablissement
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(413, 345);
+            this.Controls.Add(this.btnValider);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.txtVilleEtablissement);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtMailEtablissement);
+            this.Controls.Add(this.txtTelEtablissement);
+            this.Controls.Add(this.txtCodePostalEtablissement);
+            this.Controls.Add(this.txtNumRueEtablissement);
+            this.Controls.Add(this.txtRueEtablissement);
+            this.Controls.Add(this.txtNomEtablissement);
+            this.Name = "ImportEtablissement";
+            this.Text = "Importer un établissement";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnValider;
+
+        private System.Windows.Forms.Label label1;
+
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox txtVilleEtablissement;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtMailEtablissement;
+        private System.Windows.Forms.TextBox txtTelEtablissement;
+        private System.Windows.Forms.TextBox txtCodePostalEtablissement;
+        private System.Windows.Forms.TextBox txtNumRueEtablissement;
+        private System.Windows.Forms.TextBox txtRueEtablissement;
+        private System.Windows.Forms.TextBox txtNomEtablissement;
+
+        #endregion
+    }
+}
---
+++ CartesAcces/ImportEtablissement.cs
@@ -0,0 +1,12 @@
+using System.Windows.Forms;
+
+namespace CartesAcces
+{
+    public partial class ImportEtablissement : Form
+    {
+        public ImportEtablissement()
+        {
+            InitializeComponent();
+        }
+    }
+}
