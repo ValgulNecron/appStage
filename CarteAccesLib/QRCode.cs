2f1f0ad5ddb38f09ac480a97c92e6bcad9f06f1e
valgul <valgulnecron@gmail.com>
2023-02-01 10:09:37 +0100

    qr code

--- CarteAccesLib/QRCode.cs
+++ CarteAccesLib/QRCode.cs
@@ -1,7 +1,19 @@
-﻿namespace CarteAccesLib
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using QRCoder;
+
+
+namespace CarteAccesLib
 {
-    public class QRCode
+    public static class QRCode
     {
-        
+        static public Bitmap creationQRCode(string url)
+        {
+            var qrGenerator = new QRCodeGenerator();
+            var qrCodeData = qrGenerator.CreateQrCode(url, QRCodeGenerator.ECCLevel.Q);
+            var qrCode = new QRCoder.QRCode(qrCodeData);
+            var qrCodeImage = qrCode.GetGraphic(20);
+            return qrCodeImage;
+        }
     }
 }
