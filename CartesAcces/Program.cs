fdd604eb6defc4b837caa79d3c492e20e36d17ae
valgul <valgulnecron@gmail.com>
2023-02-05 21:19:04 +0100

    pas sentry au finale

--- CartesAcces/Program.cs
+++ CartesAcces/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Windows.Forms;
-using Sentry;
 
 namespace CartesAcces
 {
@@ -14,24 +13,13 @@ namespace CartesAcces
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.SetUnhandledExceptionMode(UnhandledExceptionMode.ThrowException);
-            using (SentrySdk.Init(o =>
-                   { 
-                       o.Dsn = "https://a4a3d0bd171f4e5a9fd0e136f7b8d973@o4504629047263232.ingest.sentry.io/4504629056438272";
-                       // When configuring for the first time, to see what the SDK is doing:
-                       o.Debug = true;
-                       // Set traces_sample_rate to 1.0 to capture 100% of transactions for performance monitoring.
-                       // We recommend adjusting this value in production.
-                       o.TracesSampleRate = 1.0;
-                       // Enable Global Mode if running in a client app
-                       o.IsGlobalModeEnabled = true;
-                   }))
-            {
-                // App code goes here. Dispose the SDK before exiting to flush events.
-                ClassSql.init();
+            try
+                {
+                    ClassSql.init();
+                }
+                catch{}
                 Globale.Accueil = new frmAccueil();
                 Application.Run(Globale.Accueil);
             }
-        }
     }
 }
