5990486920add6ab1d5751278d2e9135f9298a42
valgul <valgulnecron@gmail.com>
2023-02-05 21:59:54 +0100

    bon go tuer microsoft et leur nugget de ces mort la comment sa tu dl le package et tu doit EN PLUS set la reference a la main alors que cette PTN DE MERDE DE NUGGET DOIT LE FAIRE

--- CartesAcces/Program.cs
+++ CartesAcces/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Windows.Forms;
+using Sentry;
 
 namespace CartesAcces
 {
@@ -13,13 +14,28 @@ namespace CartesAcces
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            try
-                {
-                    ClassSql.init();
-                }
-                catch{}
-                Globale.Accueil = new frmAccueil();
-                Application.Run(Globale.Accueil);
+            using (SentrySdk.Init(o =>
+                   {
+                       o.Dsn = "https://a4a3d0bd171f4e5a9fd0e136f7b8d973@o4504629047263232.ingest.sentry.io/4504629056438272";
+                       // When configuring for the first time, to see what the SDK is doing:
+                       o.Debug = true;
+                       // Set traces_sample_rate to 1.0 to capture 100% of transactions for performance monitoring.
+                       // We recommend adjusting this value in production.
+                       o.TracesSampleRate = 1.0;
+                       // Enable Global Mode if running in a client app
+                       o.IsGlobalModeEnabled = true;
+                   }))
+            {
+                // App code goes here. Dispose the SDK before exiting to flush events.
+                try
+                                {
+                                    ClassSql.init();
+                                }
+                                catch{}
+                                Globale.Accueil = new frmAccueil();
+                                Application.Run(Globale.Accueil);
+                            }
             }
+            
     }
 }
