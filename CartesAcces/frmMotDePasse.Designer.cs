8a83291ad41354cf8982cb899e9d8bab7757b141
valgul <valgulnecron@gmail.com>
2023-02-16 19:34:38 +0100

    faudrait juste faire plus propre

--- CartesAcces/frmMotDePasse.Designer.cs
+++ CartesAcces/frmMotDePasse.Designer.cs
@@ -31,11 +31,41 @@ namespace CartesAcces
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(195, 274);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(369, 116);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(170, 88);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(373, 26);
+            this.textBox1.TabIndex = 1;
+            // 
+            // frmMotDePasse
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "frmMotDePasse";
             this.Text = "frmMotDePasse";
+            this.ResumeLayout(false);
+            this.PerformLayout();
         }
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
 
         #endregion
     }
