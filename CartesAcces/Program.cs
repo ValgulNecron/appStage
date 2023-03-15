2a49533eba1c328963570888ea691a6f5162ae01
march67 <td.ta.david@gmail.com>
2023-03-15 08:16:43 +0100

    Espacement pour que le code soit plus lisible
    Avec : David

--- CartesAcces/Program.cs
+++ CartesAcces/Program.cs
@@ -29,11 +29,12 @@ namespace CartesAcces
                 Globale.ConnectionBdd = false;
                 
                 MessageBox.Show("Connection impossible : " + e.Message);
-                // MessageBox.Show("Veuiller verifier le fichier config.xml et relancer l'application");
+                
+                // MessageBox.Show("Veuiller verifier le fichier config.xml et relancer l'application"); // ancienne version sans hypertext
+                
                 string message = "Veuiller vérifier le fichier config.xml et relancer l'application, vous pouvez également consulter le : ";
                 string hypertext = "Guide";
                 string url = "https://github.com/ValgulNecron/appStage/blob/main/file/pdf/Guide_de_l'utilisateur_-_16.02.2023.pdf";
-
                 MessagePersonnalisee.Show(message, hypertext, url);
 
             }
